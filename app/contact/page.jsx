da59b4845e292162a9d6e8e06c6508cf906a90d4
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-06-08 21:45:57 +0530

    done

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
           <div className="lg:w-[54%] order-2 lg:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">
-                Let's work together
+                Let&apos;s work together
               </h3>
               <p className="text-white/60">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, dignissimos!
